63b3743a972a9a78c43464721e4232a952f70d44
f99mlu <mats.lundquist75@gmail.com>
2024-11-26 20:33:04 +0100

    Use RemoveXML

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 cd "C:\MSFS SDK\Tools\bin"
 rmdir /s /q c:\Temp\Packages\f99mlu-finland-helipads
 fspackagetool.exe "D:\git-repos\finnish-helipads\f99mlu-finland-helipadsproject.xml" -outputdir c:\temp
-copy c:\Temp\Packages\f99mlu-finland-helipads\layout.json c:\Temp\Packages\f99mlu-finland-helipads\layout_noconflicts.json
+java -jar ..\RemoveXML.jar b c:\Temp\Packages\f99mlu-finland-helipads\layout.json c:\Temp\Packages\f99mlu-finland-helipads\layout_noconflicts.json helipads.bgl
+java -jar ..\RemoveXML.jar b c:\Temp\Packages\f99mlu-finland-helipads\layout.json c:\Temp\Packages\f99mlu-finland-helipads\layout.json helipads_noconflicts.bgl
 rmdir /s /q D:\MSFS\Community\f99mlu-finland-helipads
 xcopy /E /I c:\Temp\Packages\f99mlu-finland-helipads D:\MSFS\Community\f99mlu-finland-helipads
